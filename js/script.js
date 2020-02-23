c473b5559c35fda0722ba3d174ebc93e132c5aa4
Zzaaf <zzaafamania@gmail.com>
2020-02-23 20:12:21 +0300

    Added conditions and count

--- js/script.js
+++ js/script.js
@@ -18,7 +18,10 @@ const startButton = document.querySelector('.start-button'),
     endButton = document.querySelector('.end-button'),
     formCalculate = document.querySelector('.form-calculate'),
     totalField = document.querySelector('.total'),
-    fastRange = document.querySelector('.fast-range');
+    fastRange = document.querySelector('.fast-range'),
+    totalPriceSum = document.querySelector('.total_price__sum'),
+    adapt = document.getElementById('adapt'),
+    mobileTemplates = document.getElementById('mobileTemplates');
 
 function showElement(element) {
     element.style.display = 'block';
@@ -29,6 +32,10 @@ function hideElement(element) {
 }
 
 function priceCalculation(element) {
+    let result = 0,
+        index = 0,
+        options = [];
+
     if (element.name === 'whichSite') {
         for (const item of formCalculate.elements) {
             if (item.type === 'checkbox') {
@@ -37,6 +44,41 @@ function priceCalculation(element) {
         }
         hideElement(fastRange);
     }
+
+    if (adapt.checked == false) {
+        mobileTemplates.checked == false;
+        mobileTemplates.setAttribute("disabled", "disabled");
+    } else {
+        mobileTemplates.removeAttribute("disabled", "disabled");
+    }
+
+    for (const item of formCalculate.elements) {
+        if (item.name === 'whichSite' && item.checked) {
+            index = DATA.whichSite.indexOf(item.value)            
+        } else if (item.classList.contains('calc-handler') && item.checked) {
+            options.push(item.value)
+        }
+    }
+
+    options.forEach(function(key){
+        if (typeof(DATA[key]) === 'number') {
+            if (key === 'sendOrder') {
+                result += DATA[key];
+            } else {
+                result += DATA.price[index] * DATA[key] / 100;
+            }
+        } else {
+            if (key === 'desktopTemplates') {
+                result += DATA.price[index] * DATA[key][index] / 100;
+            } else {
+                result += DATA[key][index];
+            }
+        }
+    })    
+
+    result += DATA.price[index];
+
+    totalPriceSum.textContent = result;
 }
 
 function handler(event) {
